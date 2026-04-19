f16a2fd6d2854095c8035859830ffa1db28075e5
TakalaniMasubi <takalanimusubi117@gmail.com>
2026-04-19 12:08:10 +0200

    Fix Vercel deployment - include routes and fix static file serving

--- server.js
+++ server.js
@@ -57,12 +57,12 @@ app.get('/api', (req, res) => {
     });
 });
 
-// Serve static files from React app
+// Serve static files from root directory
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname)));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'index.html'));
     });
 }
 
